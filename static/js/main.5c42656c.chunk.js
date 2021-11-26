477bfcd87c64cc359dcd70c74170c8c7d80f0e81
Ægir Pétursson <aegir@hugsmidjan.is>
2021-11-26 21:48:15 +0000

    Updates

--- static/js/main.5c42656c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptestari=this.webpackJsonptestari||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var r=n(3),c=n.n(r),s=(n(8),n(0));var i=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"\xde\xe1 get \xe9g byrja\xf0 a\xf0 leika m\xe9r"})})};c.a.render(Object(s.jsx)("div",{children:Object(s.jsx)(i,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.5c42656c.chunk.js.map
